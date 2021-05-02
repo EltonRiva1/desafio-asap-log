d788c8f16a4eeb431ed1c96cbca40e8811a04afe
EltonRiva1 <notleamil@hotmail.com>
2021-05-02 11:46:43 -0300

    package model and class

---
+++ src/main/java/br/com/viavarejo/model/Apolice.java
@@ -0,0 +1,149 @@
+package br.com.viavarejo.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Document
+public class Apolice implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String numero;
+	@NotNull
+	private LocalDate dataInicioVigencia;
+	@NotNull
+	private LocalDate dataFimVigencia;
+	@NotNull
+	private String placaVeiculo;
+	@NotNull
+	private Double valor;
+	@NotNull
+	@DBRef // relationship MongoDB - Only apolice for only cliente or only clinte for many
+			// apolice
+	private Cliente cliente;
+
+	@JsonProperty
+	public String getNumero() {
+		return numero;
+	}
+
+	@JsonIgnore
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public LocalDate getDataInicioVigencia() {
+		return dataInicioVigencia;
+	}
+
+	public void setDataInicioVigencia(LocalDate dataInicioVigencia) {
+		this.dataInicioVigencia = dataInicioVigencia;
+	}
+
+	public LocalDate getDataFimVigencia() {
+		return dataFimVigencia;
+	}
+
+	public void setDataFimVigencia(LocalDate dataFimVigencia) {
+		this.dataFimVigencia = dataFimVigencia;
+	}
+
+	public String getPlacaVeiculo() {
+		return placaVeiculo;
+	}
+
+	public void setPlacaVeiculo(String placaVeiculo) {
+		this.placaVeiculo = placaVeiculo;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+		result = prime * result + ((dataFimVigencia == null) ? 0 : dataFimVigencia.hashCode());
+		result = prime * result + ((dataInicioVigencia == null) ? 0 : dataInicioVigencia.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		result = prime * result + ((placaVeiculo == null) ? 0 : placaVeiculo.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Apolice other = (Apolice) obj;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
+		if (dataFimVigencia == null) {
+			if (other.dataFimVigencia != null)
+				return false;
+		} else if (!dataFimVigencia.equals(other.dataFimVigencia))
+			return false;
+		if (dataInicioVigencia == null) {
+			if (other.dataInicioVigencia != null)
+				return false;
+		} else if (!dataInicioVigencia.equals(other.dataInicioVigencia))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		if (placaVeiculo == null) {
+			if (other.placaVeiculo != null)
+				return false;
+		} else if (!placaVeiculo.equals(other.placaVeiculo))
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Apolice [numero=" + numero + ", dataInicioVigencia=" + dataInicioVigencia + ", dataFimVigencia="
+				+ dataFimVigencia + ", placaVeiculo=" + placaVeiculo + ", valor=" + valor + ", cliente=" + cliente
+				+ "]";
+	}
+
+}
---
+++ src/main/java/br/com/viavarejo/model/Cliente.java
@@ -0,0 +1,128 @@
+package br.com.viavarejo.model;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Document
+public class Cliente implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String id;
+	@NotNull
+	private String nome;
+	@NotNull
+	@CPF
+	private String cpf;
+	@NotNull
+	private String cidade;
+	@NotNull
+	private String uf;
+
+	@JsonProperty
+	public String getId() {
+		return id;
+	}
+
+	@JsonIgnore
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (uf == null) {
+			if (other.uf != null)
+				return false;
+		} else if (!uf.equals(other.uf))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", cidade=" + cidade + ", uf=" + uf + "]";
+	}
+
+}
