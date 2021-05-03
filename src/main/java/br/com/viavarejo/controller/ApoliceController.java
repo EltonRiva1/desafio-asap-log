96a015352776b0e572931b7c9aa1631842feafad
EltonRiva1 <notleamil@hotmail.com>
2021-05-03 09:11:13 -0300

    Controller adjust

--- src/main/java/br/com/viavarejo/controller/ApoliceController.java
+++ src/main/java/br/com/viavarejo/controller/ApoliceController.java
@@ -20,37 +20,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.viavarejo.model.Apolice;
 import br.com.viavarejo.service.impl.ApoliceServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/apolices")
+@Api(value = "Apolice")
 public class ApoliceController {
 	@Autowired
 	private ApoliceServiceImpl apoliceServiceImpl;
 
+	@ApiOperation(value = "Create an policy")
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Apolice> create(@RequestBody(required = true) @Valid Apolice apolice) {
 		return new ResponseEntity<Apolice>(this.apoliceServiceImpl.create(apolice), HttpStatus.CREATED);
 	}
 
+	@ApiOperation(value = "Update an policy by its number")
 	@PutMapping(value = "/{numero}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Apolice> update(@RequestBody(required = true) @Valid Apolice apolice,
 			@PathVariable("numero") String numero) {
 		return new ResponseEntity<Apolice>(this.apoliceServiceImpl.update(apolice, numero), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Delete an policy by its number")
 	@DeleteMapping(value = "/{numero}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable(required = true, name = "id") String numero) {
+	public void delete(@PathVariable(required = true, name = "numero") String numero) {
 		this.apoliceServiceImpl.delete(numero);
 	}
+	
+	@ApiOperation(value = "Find by policy for your number")
+	@GetMapping(value = "/findByNumero/{numero}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Apolice> findByNumero(@PathVariable(required = true, name = "numero") String numero) {
+		return new ResponseEntity<Apolice>(this.apoliceServiceImpl.findByNumero(numero), HttpStatus.OK);
+	}
 
+	@ApiOperation(value = "List all policy")
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Apolice>> findAll() {
 		return new ResponseEntity<List<Apolice>>(this.apoliceServiceImpl.findAll(), HttpStatus.OK);
-	}
-
-	@GetMapping(value = "/findByNumero/{numero}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Apolice> findByNumero(@PathVariable(required = true, name = "id") String numero) {
-		return new ResponseEntity<Apolice>(this.apoliceServiceImpl.findByNumero(numero), HttpStatus.OK);
 	}
 }
--- src/main/java/br/com/viavarejo/controller/ClienteController.java
+++ src/main/java/br/com/viavarejo/controller/ClienteController.java
@@ -20,35 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.viavarejo.model.Cliente;
 import br.com.viavarejo.service.impl.ClienteServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/clientes")
+@Api(value = "Cliente")
 public class ClienteController {
 	@Autowired
 	private ClienteServiceImpl clienteServiceImpl;
 
+	@ApiOperation(value = "Create a client")
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> create(@RequestBody(required = true) @Valid Cliente cliente) {
 		return new ResponseEntity<Cliente>(this.clienteServiceImpl.create(cliente), HttpStatus.CREATED);
 	}
 
+	@ApiOperation(value = "Update a customer by his ID")
 	@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> update(@RequestBody(required = true) @Valid Cliente cliente,
 			@PathVariable("id") String id) {
 		return new ResponseEntity<Cliente>(this.clienteServiceImpl.update(cliente, id), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Consult a customer by ID")
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> findById(@PathVariable(required = true, name = "id") String id) {
 		return new ResponseEntity<Cliente>(this.clienteServiceImpl.findById(id), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Delete a client by her ID")
 	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable(required = true, name = "id") String id) {
 		this.clienteServiceImpl.delete(id);
 	}
 
+	@ApiOperation(value = "List all customers")
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Cliente>> findAll() {
 		return new ResponseEntity<List<Cliente>>(this.clienteServiceImpl.findAll(), HttpStatus.OK);
